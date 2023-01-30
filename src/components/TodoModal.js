b2335cd1c753a4ca047cc314dd1583ef57b2fca7
siddiq <msiddiq1400@gmail.com>
2023-01-30 07:56:19 +0500

    animation for modal

--- src/components/TodoModal.js
+++ src/components/TodoModal.js
@@ -6,6 +6,28 @@ import { useDispatch } from 'react-redux';
 import { addTodo, updateTodo } from '../slices/todoSlice';
 import {v4 as uuid} from 'uuid';
 import { toast } from 'react-hot-toast';
+import { AnimatePresence, motion } from 'framer-motion';
+
+const dropIn = {
+    hidden: {
+      opacity: 0,
+      transform: 'scale(0.9)',
+    },
+    visible: {
+      transform: 'scale(1)',
+      opacity: 1,
+      transition: {
+        duration: 0.1,
+        type: 'spring',
+        damping: 25,
+        stiffness: 500,
+      },
+    },
+    exit: {
+      transform: 'scale(0.9)',
+      opacity: 0,
+    },
+};
 
 const TodoModal = ({modalOpen,setModalOpen, type, todo}) => {
   const [title, setTitle] = useState('');
@@ -54,19 +76,22 @@ const TodoModal = ({modalOpen,setModalOpen, type, todo}) => {
   }
 
   return ( 
-    <div>    
+    <AnimatePresence>
         {modalOpen && 
-        <div className={styles.wrapper}>
-            <div className={styles.container}>
-                <div 
+        <motion.div className={styles.wrapper} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
+            <motion.div className={styles.container} variants={dropIn} initial="hidden" animate="visible" exit="exit">
+                <motion.div 
                     className={styles.closeButton} 
                     onClick={() => setModalOpen(false)} 
                     onKeyDown={() => setModalOpen(false)}
                     tabIndex={0}
                     role="button"
+                    initial={{top: 40, opacity: 0}}
+                    animate={{top: -10, opacity: 1}}
+                    exit={{top: 40, opacity: 0}}
                 >
                     <AiOutlineCloseSquare/>
-                </div>
+                </motion.div>
                 <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
                     <h1 className={styles.formTitle}>{type === 'update' ? 'Update Task' : 'Add Task'}</h1>
                     <label htmlFor='title'>
@@ -85,10 +110,10 @@ const TodoModal = ({modalOpen,setModalOpen, type, todo}) => {
                         <Button type='button' variant='secondary' onClick={() => setModalOpen(false)} onKeyDown={() => setModalOpen(false)} >Cancel</Button>
                     </div>
                 </form>
-                </div>
-            </div>
+                </motion.div>
+            </motion.div>
         }
-    </div>
+    </AnimatePresence>
   )
 }
 
